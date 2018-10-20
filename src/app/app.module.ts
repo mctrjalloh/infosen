15112aeb631c7697e23d461f01566d5f8b43ed2b
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-10-20 03:41:40 +0000

    Added article major components and basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatIconModule } from "@angular/material/icon";
+
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { ArticleComponent } from './article/article.component';
+import { ArticleComponent } from "./article/article.component";
 
 @NgModule({
   declarations: [AppComponent, ArticleComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatIconModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 
 interface Article {
+  category: string;
   title: string;
   image: string;
+  content: string;
+  likes: number;
+  comments: string[];
+  shares: number;
   author: string;
   date: string;
 }
@@ -13,6 +18,23 @@ interface Article {
   styleUrls: ["./article.component.css"]
 })
 export class ArticleComponent implements OnInit {
+  article = {
+    category: "Videos",
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    image:
+      "http://images.seneweb.com//content/seneweb/news/images/10-2018/thumbs/308x160_e872d295a1a885d558f59f86f631d3bb99176028.png",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur optio iusto sit facilis, maxime temporibus saepe tempora, consequatur quibusdam nisi ex corporis asperiores ducimus, amet beatae dignissimos. Porro, placeat sint.",
+    likes: 7,
+    comments: [
+      "Lorem ipsum dolor sit amet consectetur",
+      "Consequuntur optio iusto sit facilis",
+      "maxime temporibus saepe tempora"
+    ],
+    shares: 4,
+    author: "John Doe",
+    date: "Oct 20, 2018"
+  };
   constructor() {}
 
   ngOnInit() {}
