e838700b295c16f5995e02e1b13ae7a06389abfe
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-10-20 15:32:12 +0000

    Article as an ArticleDetail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { MatBadgeModule } from "@angular/material/badge";
 import { MatIconModule } from "@angular/material/icon";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatDividerModule } from "@angular/material/divider";
+import {MatGridListModule} from '@angular/material/grid-list';
 
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -18,7 +21,10 @@ import { ArticleComponent } from "./article/article.component";
     MatCardModule,
     MatButtonModule,
     MatBadgeModule,
-    MatIconModule
+    MatIconModule,
+    MatChipsModule,
+    MatDividerModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -4,12 +4,14 @@ interface Article {
   category: string;
   title: string;
   image: string;
+  image_alt: string;
   content: string;
   likes: number;
   comments: string[];
   shares: number;
   author: string;
   date: string;
+  tags: string[];
 }
 
 @Component({
@@ -23,6 +25,7 @@ export class ArticleComponent implements OnInit {
     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     image:
       "http://images.seneweb.com//content/seneweb/news/images/10-2018/thumbs/308x160_e872d295a1a885d558f59f86f631d3bb99176028.png",
+    image_alt: "src/assets/img/bassirou.png",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur optio iusto sit facilis, maxime temporibus saepe tempora, consequatur quibusdam nisi ex corporis asperiores ducimus, amet beatae dignissimos. Porro, placeat sint.",
     likes: 7,
@@ -33,7 +36,8 @@ export class ArticleComponent implements OnInit {
     ],
     shares: 4,
     author: "John Doe",
-    date: "Oct 20, 2018"
+    date: "Oct 20, 2018",
+    tags: ["lorem", "ipsum", "dolor", "sit"]
   };
   constructor() {}
 
